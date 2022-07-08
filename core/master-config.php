0c80e3dcc6b74524909852ed064b3ad0551fa846
Chintan <care@divaasolutions.com>
2022-07-08 09:37:42 +0530

    Update master-config.php

--- core/master-config.php
+++ core/master-config.php
@@ -1,5 +1,5 @@
 <?php
-defined('CORE_VERSION')     OR define('CORE_VERSION', '0.1.44');
+defined('CORE_VERSION')     OR define('CORE_VERSION', '0.1.48');
 
 defined('COMPOSER_PATH')    OR define('COMPOSER_PATH', BASEPATH . '../vendor/autoload.php');
 defined('LOG_PATH')         OR define('LOG_PATH', BASEPATH . '../projects/' . PROJECT_CODE . '/logs/');
